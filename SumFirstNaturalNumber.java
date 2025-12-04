7308e2780cd7a30804854c0ef08ab143055fd906
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-12-04 11:36:08 +0530

    sum of first natural number using recursion

---
+++ SumFirstNaturalNumber.java
@@ -0,0 +1,16 @@
+public class SumFirstNaturalNumber {
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int fnm1 = sum(n - 1);
+        int sum = n + fnm1;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(sum(n));
+    }
+}
