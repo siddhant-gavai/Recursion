29b3ff1a4452904a0f9a4c7a897d22853e7be917
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-12-07 13:30:30 +0530

    change the logic

--- FibonacciRecursion.java
+++ FibonacciRecursion.java
@@ -1,14 +1,23 @@
 public class FibonacciRecursion {
 
+    // public static int fib(int n) {
+    // if (n < 2)
+    // return n;
+
+    // return fib(n - 1) + fib(n - 2);
+    // }
+
     public static int fib(int n) {
         if (n < 2)
             return n;
-
-        return fib(n - 1) + fib(n - 2);
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
     }
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         System.out.println(fib(n)); // Output: 55
     }
 }
