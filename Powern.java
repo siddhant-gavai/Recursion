f9859d8ec4c2a6283e78f50145fa1972d07e6d9c
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-12-14 11:26:56 +0530

    optimized approch

--- Powern.java
+++ Powern.java
@@ -10,7 +10,22 @@ public class Powern {
         return x * power(x, n - 1);
     }
 
+    public static int optimizedPower(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int halfPowerSq = optimizedPower(a, n / 2) * optimizedPower(a, n / 2);
+
+        // if n is odd
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+        return halfPowerSq;
+    }
+
     public static void main(String[] args) {
+        int a = 2;
+        int n = 10;
         System.out.println(power(2, 5));
     }
 }
